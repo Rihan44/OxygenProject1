fd50c52b7ae588c744e94148f77505edd4ab4ce7
Rihan44 <asmuela_bustamante@hotmail.es>
2023-08-21 23:10:38 +0200

    feat modal

--- js/main.js
+++ js/main.js
@@ -5,6 +5,21 @@ let menu = document.querySelector('#menu');
 let percentage__scroll = document.querySelector('#percentage__scroll');
 let backTop = document.querySelector('#backTop');
 let btn_form = document.querySelector('#btn_form');
+let name_form = document.querySelector('#name');
+let email = document.querySelector('#email');
+let check = document.querySelector('#check');
+let check_error = document.querySelector('.check-error');
+let div_modal = document.querySelector('#modal');
+let email_modal = document.querySelector('#email_modal');
+let btn_modal = document.querySelector('#btn_modal');
+let modal_icon = document.querySelector('#modal__iconclose');
+let form_modal = document.querySelector('#form__modal');
+let h3_modal = document.querySelector('.modal__title');
+let label_modal = document.querySelector('.label__modal');
+let localstorage_modal = localStorage.getItem("modal_cerrado");
+
+const patron_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 /* MENU BURGUER */
 
 burguer_icon.addEventListener('click', () => {
@@ -42,33 +57,104 @@ function backtopButton() {
     });
 }
 
+function formValidation(){
+    if(name_form.value.length < 2 || name_form.value.length > 100){
+        name_form.classList.add("form-error");
+    } else {
+        name_form.classList.remove("form-error");
+    }
+
+    if(!emailValidation(email.value)){
+        email.classList.add("form-error");
+    } else {
+        email.classList.remove("form-error");
+    }
+
+    if(!check.checked){
+        check_error.style.display = 'block';
+    } else {
+        check_error.style.display = 'none';
+    }
+}
+
+function emailValidation(email) {
+    return patron_email.test(email);
+}
+
 window.addEventListener('scroll', () => {
     let porcentaje = porcentajeTotal();
     percentage__scroll.style.height = "5px";
     percentage__scroll.style.width = porcentaje + "%";
+
+    /* MODAL APARECER AL 25% DE LA WEB Y SI NO ESTÁ EN EL LOCALSTORAGE */
+    if(porcentaje == 25 && !localstorage_modal){
+        div_modal.style.display = "block";
+    }
+
 });
 
 backTop.addEventListener('click', backtopButton);
 
 btn_form.addEventListener('click', (e) => {
     e.preventDefault();
-
-    let name = document.querySelector('#name');
-    console.log(name.value)
+    formValidation();
 });
 
-/* Implementar validación en el formulario. El nombre tiene que tener entre 2 y
-100 letras, la dirección de correo electrónico tiene que ser válida
-(https://www.emailregex.com/) y tienen que hacer el checkbox. Si un campo
-no es válido, cambiar el color de su border a rojo
+/* MODAL */
+/* APARECE A LOS 5s */
+if(!localstorage_modal){
+    setTimeout(() => {
+        div_modal.style.display = "block";
+    }, 5000);
+} 
+
+/* AL SUBSCRIBIRSE */
+btn_modal.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if(!emailValidation(email_modal)){
+        email_modal.style.borderBottom = "1px solid red";
+    } else {
+        email_modal.style.borderBottom = "1px solid black";
+    }
+
+});
+
+/* MODAL CERRADO CON LA X */
+modal_icon.addEventListener('click', () => {
+    div_modal.style.display = "none";
+    localStorage.setItem("modal_cerrado", div_modal);
+});
+
+/* MODAL CERRADO CON EL ESC */
+window.addEventListener('keydown', (e) => {
+    if(e.key == "Escape"){
+        div_modal.style.display = "none";
+        localStorage.setItem("modal_cerrado", div_modal);
+    }
+});
+
+/* MODAL CERRADO CLICANDO FUERA */
+window.addEventListener('click', (e) => {
+    if( e.target != form_modal && e.target != div_modal && e.target != h3_modal 
+        && e.target != email_modal && e.target != btn_modal && e.target != label_modal){
+        div_modal.style.display = "none";
+        localStorage.setItem("modal_cerrado", div_modal);
+    }
+});
+
+/* window.onload = () => {
+    localStorage.clear();
+}; */
+/* FIN DE MODAL */
+
+/* SLIDER */
+
+
+/* FIN SLIDER */
+/*
 ● Recoger los datos del formulario y mandarselos a un servidor JSON de
 testing como este https://jsonplaceholder.typicode.com/guide/ con fetch()
-● Crear un popup (/modal) de ‘Subscribe to our newsletter’ que aparece
-después de 5 segundos, o cuando el usuario baja 25% de la página. Validar
-la dirección y mandársela al mismo servidor. Habrá 3 maneras de quitarlo,
-con un botón X, haciendo clic fuera del modal o con la tecla ESC. No
-aparecerá otra vez, después de ser cerrado (con
-localStorage/sessionStorage).
 ● Añadir un selector de moneda (EUR, USD, GBP), obtener los tipos de cambio
 de esta API https://github.com/fawazahmed0/currency-api#readme
 (https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur
